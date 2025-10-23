db727c715a6b3fb4b8dd50df8a2aaf49a7d7fc4b
susil <sobhanasusil_suresh@condenast.com>
2025-10-23 15:40:33 +0530

    Fix TypeScript export errors in db package
    - Used export type for interfaces to resolve isolatedModules error
    - Removed schema exports that are not exported from source files
    - TypeScript compilation should now succeed for db package

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,6 +2,9 @@ export * from './connection';
 export * from './types';
 
 // Export models and interfaces with explicit names
-export { ITenant, Tenant, tenantSchema } from './tenant';
-export { IUser, User, userSchema } from './user';
-export { ICourse, Course, courseSchema } from './course';
+export type { ITenant } from './tenant';
+export { Tenant } from './tenant';
+export type { IUser } from './user';
+export { User } from './user';
+export type { ICourse } from './course';
+export { Course } from './course';
